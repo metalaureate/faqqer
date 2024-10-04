3a67cdf00efb3454181591627f6b2976203c475f
Simon Hill <simonhill303@gmail.com>
2024-10-04 16:15:49 -0600

    chore: update faq

--- faqqer_bot.py
+++ faqqer_bot.py
@@ -30,7 +30,7 @@ with open("avoidance_"+faq_file_path, 'r', encoding='utf-8') as faq_file:
 # Function to query OpenAI GPT-4o and handle any API errors
 def query_openai_gpt(system,faq_avoidance_text,prompt):
 
-    system=system + "\n\nDo not talk about the following topics:\n" +faq_avoidance_text+ "\n\nIf you do not know the answer with certainty, tell the user that their question will be forwarded to support staff for answering.\n\nIf the questions seems missing, remind the user that the format for interacting with you is /faqqer <question> in their preferred language. Give an example, e.g., /faqqer What is Tari Universe?"
+    system=system + "\n\nDo not talk about the following topics:\n" +faq_avoidance_text+ "\n\nIf you do not know the answer with certainty, tell the user that their question will be forwarded to support staff for answering.\n\nIf the questions seems missing, remind the user that the format for interacting with you is /faqqer <question>. Give an example, e.g., /faqqer What is Tari Universe?"
     try:
         client = OpenAI()
         response = client.chat.completions.create(
@@ -66,9 +66,9 @@ def find_faq_answer(question):
     prompt = """
     Search the FAQ for the answer.
     Avoid mentioning banned topics.
-    If the question is not in English, please interpret the question correctly and translate the answer correctly into the user's language, using idioms appropriate to the technical subject matter. 
+     
     Question:  %s
-    Answer in the user's language in JSON format: {'answer': '<answer>'}
+    Answer in JSON format: {'answer': '<answer>'}
     """ % question
 
     # Get the response from OpenAI GPT-4o
